4629c5499ac1b1f58964801f6f63a148fa4330d2
Zülfü Mehmet ÖZÇİFÇİ <zulfumehmet@gmail.com>
2024-07-09 14:57:19 +0300

    Update captiveportal-tc.php
    Tc kimlik sorgulamasında kullanıcı varsa yeni bir user kaydı açmayıp o userden devam edtmesi için revize ettim.

--- ghostportal/captiveportal-tc.php
+++ ghostportal/captiveportal-tc.php
@@ -18,11 +18,17 @@ function rand_numstring( $length ) {
 
 }
 
+function karakter_duzeltme($gelen){
+    $karakterler = array("ç","ğ","ı","i","ö","ş","ü");
+    $degistir = array("Ç","Ğ","I","İ","Ö","Ş","Ü");
+    return str_replace($karakterler, $degistir, $gelen);
+}
 
-
-
-
-
+function karakter_duzeltme2($gelen){
+    $karakterler = array("ç","Ç","ğ","Ğ","ı","İ","ö","Ö","ş","Ş","ü","Ü");
+    $degistir = array("c","c","g","g","i","i","o","o","s","s","u","u");
+    return str_replace($karakterler, $degistir, strtolower($gelen));
+}
 
 if(isset($_POST["tc"])){
 
@@ -52,10 +58,31 @@ if(isset($_POST["tc"])){
 			$bugun = date("Y-m-d");
 			$yenitarih = strtotime($expdate.' day',strtotime($bugun));
 			$tarih = date('j M Y' ,$yenitarih );
-			mysqli_query($baglan,"INSERT INTO radcheck(username,attribute,op,value,tip,tcno,adsoyad,tarih,sifre,dtarih) values('".$tc_no."','Cleartext-Password',':=','".$dogum_yili."',2,'".$tc_no."','".$ad.' '.$soyad."', '".$bugun."','".$dogum_yili."','".$dogum_yili."')");
+            // Kullanıcının veritabanında olup olmadığını kontrol eden sorgu
+            $checkUserQuery = "SELECT * FROM radcheck WHERE username = '".$tc_no."'";
+            $result = mysqli_query($baglan, $checkUserQuery);
+
+            if (mysqli_num_rows($result) == 0) {
+                // Kullanıcı mevcut değilse, yeni kayıt ekleyin
+                $insertQuery = "INSERT INTO radcheck (username, attribute, op, value, tip, tcno, adsoyad, tarih, sifre, dtarih) 
+                                VALUES ('".$tc_no."', 'Cleartext-Password', ':=', '".$dogum_yili."', 2, '".$tc_no."', '".$ad.' '.$soyad."', '".$bugun."', '".$dogum_yili."', '".$dogum_yili."')";
+
+                if (mysqli_query($baglan, $insertQuery)) {
+                    echo $tc_no.",".$dogum_yili;
+                } else {
+                    echo "Hata: " . $insertQuery . "<br>" . mysqli_error($baglan);
+                }
+            } else {
+                echo $tc_no.",".$dogum_yili;
+            }
+
+
+
+
+			//mysqli_query($baglan,"INSERT INTO radcheck(username,attribute,op,value,tip,tcno,adsoyad,tarih,sifre,dtarih) values('".$tc_no."','Cleartext-Password',':=','".$dogum_yili."',2,'".$tc_no."','".$ad.' '.$soyad."', '".$bugun."','".$dogum_yili."','".$dogum_yili."')");
 			//mysqli_query($baglan,"INSERT INTO radcheck(username,attribute,op,value) values('".$tc_no."','Expiration',':=', '".$tarih."')");
             // eger islem basariliysa -->
-            echo $tc_no.",".$dogum_yili;
+           
 			//echo "Not alınız. kadi: " . $user.', şifreniz: ' .$pass;
 
         }else {
@@ -67,16 +94,6 @@ if(isset($_POST["tc"])){
     }
 }
 
-function karakter_duzeltme($gelen){
-    $karakterler = array("ç","ğ","ı","i","ö","ş","ü");
-    $degistir = array("Ç","Ğ","I","İ","Ö","Ş","Ü");
-    return str_replace($karakterler, $degistir, $gelen);
-}
 
-function karakter_duzeltme2($gelen){
-    $karakterler = array("ç","Ç","ğ","Ğ","ı","İ","ö","Ö","ş","Ş","ü","Ü");
-    $degistir = array("c","c","g","g","i","i","o","o","s","s","u","u");
-    return str_replace($karakterler, $degistir, strtolower($gelen));
-}
 
 ?>
